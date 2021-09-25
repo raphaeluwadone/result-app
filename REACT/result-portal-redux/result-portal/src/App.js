e70cebdfa2faeb5967ec79f118be5034f771917c
raphaeluwadone <raphaeluwadone@gmail.com>
2021-09-25 09:32:01 +0100

    Site revamp

--- REACT/result-portal-redux/result-portal/src/App.js
+++ REACT/result-portal-redux/result-portal/src/App.js
@@ -98,7 +98,7 @@ const Container = styled.div`
   background-repeat: no-repeat;
   width: 100vw;
   height: 100vh;
-  padding: 30px;
+  /* padding: 30px; */
   // margin: 80px auto;
   // padding: 50px;
   box-sizing: border-box;
